6c5bf99b0064dcb7c57c03405f7e1408d9690631
Anantaraj Katti <anantarajkatti@gmail.com>
2024-05-31 13:22:07 +0530

    updated

--- index.js
+++ index.js
@@ -1,67 +1,66 @@
-const express = require('express')// third party package
-const fs = require('fs');
-const app = express()   //assigned
- const PORT=4000;
+const express = require("express"); // third party package
+const fs = require("fs");
+const app = express(); //assigned
+const PORT = 4000;
 
- const filePath = './inbuilt/current_date_time.txt';
+const filePath = "./inbuilt/current_date_time.txt";
 
-
-
-
-
-app.get('/', function (req, res) {         // http method (root end point)
-  res.send('Hello this node js task <hr/>1) /datetime<br/> to create current date-time file<br/> <br/>2)/datetime/read <br/> for reading the Date file<br/><br/>3) /retrive<br/> for reading directory')                  // what response to be sent
-})    
-
+app.get("/", function (req, res) {
+  // http method (root end point)
+  res.send(
+    "Hello this node js task <hr/>1) /datetime<br/> to create current date-time file<br/> <br/>2)/datetime/read <br/> for reading the Date file<br/><br/>3) /retrive<br/> for reading directory"
+  ); // what response to be sent
+});
 
 // creating new api end point to wtitw a text file in perticular folder
-app.get('/datetime', function (req, res) { 
- // Create a new Date object
-    const timeStamp = new Date();
+app.get("/datetime", function (req, res) {
+  // Create a new Date object
+  const timeStamp = new Date();
 
-// Convert to Indian Standard Time (IST)
-    const options = {
-    timeZone: 'Asia/Kolkata',
+  // Convert to Indian Standard Time (IST)
+  const options = {
+    timeZone: "Asia/Kolkata",
     hour12: false, // Use 24-hour format
-    weekday: 'long', // Display full weekday name
-    year: 'numeric', // Display year
-    month: 'short', // Display short month name
-    day: '2-digit', // Display day with leading zero
-    hour: '2-digit', // Display hour with leading zero
-    minute: '2-digit', // Display minute with leading zero
-    second: '2-digit' // Display second with leading zero
-    };
+    weekday: "long", // Display full weekday name
+    year: "numeric", // Display year
+    month: "short", // Display short month name
+    day: "2-digit", // Display day with leading zero
+    hour: "2-digit", // Display hour with leading zero
+    minute: "2-digit", // Display minute with leading zero
+    second: "2-digit", // Display second with leading zero
+  };
 
-    const timestampIST = timeStamp.toLocaleString('en-IN', options);
+  const timestampIST = timeStamp.toLocaleString("en-IN", options);
 
-    console.log(timestampIST);
+  console.log(timestampIST);
 
-        fs.writeFile("./inbuilt/current_date_time.txt", timestampIST , (err) => {
-        console.log("Completed writing current_date_time.txt") 
-        res.send("Completed writing current_date_time.txt <br/><br/> now provide <br/><br/> URL:-----http://localhost:4000/datetime/read ") })       
-                    
-  }) 
-
-  //--------------------------------------------Read Directory shows all files in it----------------------------
-  app.get('/retrive', function (req, res) { 
-    fs.readdir("./inbuilt", (err, files) => {
-        console.log("All file names are", files)
-        res.send(files)
-    })
-})
-
-//------------------------------
-
-  app.get('/datetime/read', function (req, res) {
-    const fs = require('fs');  
-  fs.readFile('./inbuilt/current_date_time.txt',"utf-8" ,(err, data) => {
-      
-    if (err) {
-      console.error('Error reading file:', err);
-    } 
-      console.log('Successfully read timestamp:', data);
-     res.send(data); // Send the timestamp data as the response
+  fs.writeFile("./inbuilt/current_date_time.txt", timestampIST, (err) => {
+    console.log("Completed writing current_date_time.txt");
+    res.send(
+      "Completed writing current_date_time.txt <br/><br/> now provide <br/><br/> URL:-----http://localhost:4000/datetime/read "
+    );
   });
- });
+});
 
-app.listen(PORT,()=>console.log("server is running on PORT", PORT))                          // you can give any number
+//--------------------------------------------Read Directory shows all files in it----------------------------
+app.get("/retrive", function (req, res) {
+  fs.readdir("./inbuilt", (err, files) => {
+    console.log("All file names are", files);
+    res.send(files);
+  });
+});
+
+//------------------------------//datetime/read
+
+app.get("/datetime/read", function (req, res) {
+  const fs = require("fs");
+  fs.readFile("./inbuilt/current_date_time.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+    }
+    console.log("Successfully read timestamp:", data);
+    res.send(data); // Send the timestamp data as the response
+  });
+});
+
+app.listen(PORT, () => console.log("server is running on PORT", PORT)); // you can give any number
